9d91035e5d6ddf7bcee252d0e34846ec81681b9f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-07-12 01:10:45 +0300

    setup: initial commit

---
+++ src/themes/base/night.ts
@@ -0,0 +1,10 @@
+const night = {
+    backgroundColor: 'hsl(220, 2%, 4%)',
+    backgroundColorSecondary: 'hsl(220, 2%, 14%)',
+    backgroundColorHover: 'hsla(220, 2%, 44%, 0.3)',
+    color: 'hsl(220, 2%, 87%)',
+    colorSecondary: 'hsl(220, 2%, 67%)',
+};
+
+
+export default night;
