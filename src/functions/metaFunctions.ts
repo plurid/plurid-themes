74c40409b4e9ad84922cf7dcc24d166b29322b0e
Jan Cavel <caveljan@gmail.com>
2019-07-12 01:02:25 +0300

    setup: initial commit

---
+++ src/functions/metaFunctions.ts
@@ -0,0 +1,21 @@
+export const debounce = (func: any, wait: any, immediate: boolean = false) => {
+    let timeout: any;
+
+    return function (this: any) {
+        let context = this;
+        let args = arguments;
+
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+
+        let callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(later, wait);
+
+        if (callNow) func.apply(context, args);
+    };
+};
