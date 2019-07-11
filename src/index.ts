663f7a916d4d1a68a86c451245e894f22b52ec93
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-07-12 01:02:25 +0300

    setup: initial commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import {
+    placeCaretAtEnd as _placeCaretAtEnd,
+} from './functions/dom';
+
+import {
+    copyToClipboard as _copyToClipboard,
+} from './functions/clipboard';
+
+import {
+    debounce as _debounce,
+} from './functions/metaFunctions';
+
+import {
+    uuidv4 as _uuidv4,
+} from './functions/uuid';
+
+import {
+    loadImage as _loadImage,
+} from './functions/image';
+
+import {
+    randomBetween as _randomBetween,
+} from './functions/number';
+
+
+
+export const placeCaretAtEnd = _placeCaretAtEnd;
+export const copyToClipboard = _copyToClipboard;
+export const debounce = _debounce;
+export const uuidv4 = _uuidv4;
+export const loadImage = _loadImage;
+export const randomBetween = _randomBetween;
