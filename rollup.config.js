46e5774fc3c789e706362bcf9c88abd68d3e17d7
Jan Cavel <caveljan@gmail.com>
2019-09-05 15:21:31 +0300

    update: package

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 
 
@@ -24,7 +24,7 @@ export default {
             format: 'es',
             exports: 'named',
             sourcemap: false,
-        }
+        },
     ],
     plugins: [
         external(),
