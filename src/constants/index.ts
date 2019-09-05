aaa9e34b4f1bb5b3347b4ce59d805ddfaa5c5f4e
Jan Cavel <caveljan@gmail.com>
2019-09-05 14:22:54 +0300

    update: package

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,3 +10,20 @@ export const THEME_NAME_LIGHT = 'light';
 export const THEME_NAME_PLURID = 'plurid';
 export const THEME_NAME_DEPICT = 'depict';
 export const THEME_NAME_DEVIEW = 'deview';
+
+
+export const THEME_TYPES = {
+    dark: THEME_TYPE_DARK,
+    bright: THEME_TYPE_BRIGHT,
+}
+
+export const THEME_NAMES = {
+    night: THEME_NAME_NIGHT,
+    dusk: THEME_NAME_DUSK,
+    dawn: THEME_NAME_DAWN,
+    light: THEME_NAME_LIGHT,
+
+    plurid: THEME_NAME_PLURID,
+    depict: THEME_NAME_DEPICT,
+    deview: THEME_NAME_DEVIEW,
+}
