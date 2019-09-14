52d88861b92b62d794f9fa4ec0a36f063ab3e4dd
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-14 13:04:22 +0300

    update: package

--- src/functions/uuid.ts
+++ src/functions/uuid.ts
@@ -1,6 +1,7 @@
-export const uuidv4 = () => {
+export const uuidv4 = (separator: string = '') => {
     // from https://stackoverflow.com/a/2117523
-    return ([1e7] as any + 1e3 + 4e3 + 8e3 + 1e11).replace(/[018]/g, (c:any) =>
-        (((c ^ crypto.getRandomValues(new Uint8Array(1))[0]) & 15) >> c / 4).toString(16)
+    return (
+        [1e7] as any + separator + 1e3 + separator + 4e3 + separator + 8e3 + separator + 1e11).replace(/[018]/g,
+        (c: any) => (((c ^ crypto.getRandomValues(new Uint8Array(1))[0]) & 15) >> c / 4).toString(16)
     );
 }
