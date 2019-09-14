b881487ba2b2c2f7b3f3a968b452f7f7e5e8a4b6
Jan Cavel <caveljan@gmail.com>
2019-09-14 13:04:22 +0300

    update: package

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,39 @@
 import {
-    placeCaretAtEnd as _placeCaretAtEnd,
+    placeCaretAtEnd,
 } from './functions/dom';
 
 import {
-    copyToClipboard as _copyToClipboard,
+    copyToClipboard,
 } from './functions/clipboard';
 
 import {
-    debounce as _debounce,
+    debounce,
 } from './functions/metaFunctions';
 
 import {
-    uuidv4 as _uuidv4,
+    uuidv4,
 } from './functions/uuid';
 
 import {
-    loadImage as _loadImage,
+    loadImage,
 } from './functions/image';
 
 import {
-    randomBetween as _randomBetween,
+    randomBetween,
 } from './functions/number';
 
 
 
-export const placeCaretAtEnd = _placeCaretAtEnd;
-export const copyToClipboard = _copyToClipboard;
-export const debounce = _debounce;
-export const uuidv4 = _uuidv4;
-export const loadImage = _loadImage;
-export const randomBetween = _randomBetween;
+export {
+    placeCaretAtEnd,
+
+    copyToClipboard,
+
+    debounce,
+
+    uuidv4,
+
+    loadImage,
+
+    randomBetween,
+}
