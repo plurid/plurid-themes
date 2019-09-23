183545878ff15e25d9d825c252cc7f36e9719a7a
Jan Cavel <caveljan@gmail.com>
2019-09-23 12:23:15 +0300

    update: package

--- src/themes/product/plurid.ts
+++ src/themes/product/plurid.ts
@@ -4,58 +4,58 @@ import {
 
 import {
     THEME_NAME_PLURID,
-    THEME_TYPE_BRIGHT,
+    THEME_TYPE_DARK,
 } from '../../constants';
 
 
 
 const plurid: Theme = {
-    type: THEME_TYPE_BRIGHT,
+    type: THEME_TYPE_DARK,
     name: THEME_NAME_PLURID,
 
-    baseColor: 'hsl(247, 19%, 39%)',
-    baseColorInverted: 'hsl(247, 19%, 61%)',
+    baseColor: 'hsl(220, 10%, 32%)',
+    baseColorInverted: 'hsl(220, 10%, 68%)',
 
-    backgroundColorDark: 'hsl(247, 19%, 10%)',
-    backgroundColorBright: 'hsl(247, 19%, 90%)',
+    backgroundColorDark: 'hsl(220, 10%, 10%)',
+    backgroundColorBright: 'hsl(220, 10%, 90%)',
 
-    backgroundColorPrimary: 'hsl(247, 19%, 39%)',
-    backgroundColorPrimaryAlpha: 'hsla(247, 19%, 39%, 0.4)',
-    backgroundColorPrimaryInverted: 'hsl(247, 19%, 61%)',
+    backgroundColorPrimary: 'hsl(220, 10%, 32%)',
+    backgroundColorPrimaryAlpha: 'hsla(220, 10%, 32%, 0.4)',
+    backgroundColorPrimaryInverted: 'hsl(220, 10%, 68%)',
 
-    backgroundColorSecondary: 'hsl(247, 19%, 29%)',
-    backgroundColorSecondaryAlpha: 'hsla(247, 19%, 29%, 0.4)',
-    backgroundColorSecondaryInverted: 'hsl(247, 19%, 71%)',
+    backgroundColorSecondary: 'hsl(220, 10%, 26%)',
+    backgroundColorSecondaryAlpha: 'hsla(220, 10%, 26%, 0.4)',
+    backgroundColorSecondaryInverted: 'hsl(220, 10%, 74%)',
 
-    backgroundColorTertiary: 'hsl(247, 19%, 19%)',
-    backgroundColorTertiaryAlpha: 'hsla(247, 19%, 19%, 0.4)',
-    backgroundColorTertiaryInverted: 'hsl(247, 19%, 81%)',
+    backgroundColorTertiary: 'hsl(220, 10%, 18%)',
+    backgroundColorTertiaryAlpha: 'hsla(220, 10%, 18%, 0.4)',
+    backgroundColorTertiaryInverted: 'hsl(220, 10%, 82%)',
 
-    backgroundColorQuaternary: 'hsl(247, 19%, 9%)',
-    backgroundColorQuaternaryAlpha: 'hsla(247, 19%, 9%, 0.4)',
-    backgroundColorQuaternaryInverted: 'hsl(247, 19%, 91%)',
+    backgroundColorQuaternary: 'hsl(220, 10%, 12%)',
+    backgroundColorQuaternaryAlpha: 'hsla(220, 10%, 12%, 0.4)',
+    backgroundColorQuaternaryInverted: 'hsl(220, 10%, 88%)',
 
-    colorPrimary: 'hsl(247, 7%, 97%)',
-    colorPrimaryInverted: 'hsl(247, 7%, 3%)',
+    colorPrimary: 'hsl(220, 10%, 97%)',
+    colorPrimaryInverted: 'hsl(220, 10%, 3%)',
 
-    colorSecondary: 'hsl(247, 7%, 80%)',
-    colorSecondaryInverted: 'hsl(247, 7%, 20%)',
+    colorSecondary: 'hsl(220, 10%, 80%)',
+    colorSecondaryInverted: 'hsl(220, 10%, 20%)',
 
-    colorTertiary: 'hsl(247, 7%, 60%)',
-    colorTertiaryInverted: 'hsl(247, 7%, 40%)',
+    colorTertiary: 'hsl(220, 10%, 60%)',
+    colorTertiaryInverted: 'hsl(220, 10%, 40%)',
 
 
-    boxShadowUmbra: '0px 3px 6px 0px hsla(247, 94%, 10%, 0.8)',
-    boxShadowUmbraColor: 'hsla(247, 94%, 10%, 0.8)',
-    boxShadowUmbraInset: 'inset 0px -3px 4px 0px hsla(247, 94%, 10%, 0.9)',
+    boxShadowUmbra: '0px 3px 6px 0px hsla(220, 10%, 10%, 0.8)',
+    boxShadowUmbraColor: 'hsla(220, 10%, 10%, 0.8)',
+    boxShadowUmbraInset: 'inset 0px -3px 4px 0px hsla(220, 10%, 10%, 0.9)',
 
-    boxShadowPenumbra: '0px 3px 12px 0px hsla(247, 94%, 10%, 0.8)',
-    boxShadowPenumbraColor: 'hsla(247, 94%, 10%, 0.8)',
-    boxShadowPenumbraInset: 'inset 0px -3px 6px 0px hsla(247, 94%, 10%, 0.9)',
+    boxShadowPenumbra: '0px 3px 12px 0px hsla(220, 10%, 10%, 0.8)',
+    boxShadowPenumbraColor: 'hsla(220, 10%, 10%, 0.8)',
+    boxShadowPenumbraInset: 'inset 0px -3px 6px 0px hsla(220, 10%, 10%, 0.9)',
 
-    boxShadowAntumbra: '0px 3px 18px 0px hsla(247, 94%, 10%, 0.8)',
-    boxShadowAntumbraColor: 'hsla(247, 94%, 10%, 0.8)',
-    boxShadowAntumbraInset: 'inset 0px -3px 8px 0px hsla(247, 94%, 10%, 0.9)',
+    boxShadowAntumbra: '0px 3px 18px 0px hsla(220, 10%, 10%, 0.8)',
+    boxShadowAntumbraColor: 'hsla(220, 10%, 10%, 0.8)',
+    boxShadowAntumbraInset: 'inset 0px -3px 8px 0px hsla(220, 10%, 10%, 0.9)',
 };
 
 
