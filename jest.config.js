1bc942468fdfd22188f43a82a444254451bf6caa
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-27 11:41:25 +0300

    setup: cleanup functions

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    roots: ['<rootDir>/src'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__specs__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}
