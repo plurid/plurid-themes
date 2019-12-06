1b7d93e7e373381b5314f47aa2ccbc41e8deec01
Jan Cavel <caveljan@gmail.com>
2019-12-06 16:20:58 +0200

    dev: dewiki

--- source/themes/product/dewiki.ts
+++ source/themes/product/dewiki.ts
@@ -3,37 +3,37 @@ import {
 } from '../../';
 
 import {
+    THEME_TYPE_DARK,
     THEME_NAME_DEWIKI,
-    THEME_TYPE_BRIGHT,
 } from '../../constants';
 
 
 
 const dewiki: Theme = {
-    type: THEME_TYPE_BRIGHT,
+    type: THEME_TYPE_DARK,
     name: THEME_NAME_DEWIKI,
 
-    baseColor: 'hsl(0, 89%, 77%)',
-    baseColorInverted: 'hsl(0, 89%, 23%)',
+    baseColor: 'hsl(0, 81%, 33%)',
+    baseColorInverted: 'hsl(0, 81%, 67%)',
 
-    backgroundColorDark: 'hsl(0, 89%, 10%)',
-    backgroundColorBright: 'hsl(0, 89%, 90%)',
+    backgroundColorDark: 'hsl(0, 81%, 10%)',
+    backgroundColorBright: 'hsl(0, 81%, 90%)',
 
-    backgroundColorPrimary: 'hsl(0, 89%, 77%)',
-    backgroundColorPrimaryAlpha: 'hsla(0, 89%, 77%, 0.4)',
-    backgroundColorPrimaryInverted: 'hsl(0, 89%, 23%)',
+    backgroundColorPrimary: 'hsl(0, 81%, 33%)',
+    backgroundColorPrimaryAlpha: 'hsla(0, 81%, 33%, 0.4)',
+    backgroundColorPrimaryInverted: 'hsl(0, 81%, 67%)',
 
-    backgroundColorSecondary: 'hsl(0, 89%, 67%)',
-    backgroundColorSecondaryAlpha: 'hsla(0, 89%, 67%, 0.4)',
-    backgroundColorSecondaryInverted: 'hsl(0, 89%, 33%)',
+    backgroundColorSecondary: 'hsl(0, 81%, 43%)',
+    backgroundColorSecondaryAlpha: 'hsla(0, 81%, 43%, 0.4)',
+    backgroundColorSecondaryInverted: 'hsl(0, 81%, 57%)',
 
-    backgroundColorTertiary: 'hsl(0, 89%, 60%)',
-    backgroundColorTertiaryAlpha: 'hsla(0, 89%, 60%, 0.4)',
-    backgroundColorTertiaryInverted: 'hsl(0, 89%, 40%)',
+    backgroundColorTertiary: 'hsl(0, 81%, 52%)',
+    backgroundColorTertiaryAlpha: 'hsla(0, 81%, 52%, 0.4)',
+    backgroundColorTertiaryInverted: 'hsl(0, 81%, 48%)',
 
-    backgroundColorQuaternary: 'hsl(0, 89%, 50%)',
-    backgroundColorQuaternaryAlpha: 'hsla(0, 89%, 50%, 0.4)',
-    backgroundColorQuaternaryInverted: 'hsl(0, 89%, 50%)',
+    backgroundColorQuaternary: 'hsl(0, 81%, 60%)',
+    backgroundColorQuaternaryAlpha: 'hsla(0, 81%, 60%, 0.4)',
+    backgroundColorQuaternaryInverted: 'hsl(0, 81%, 40%)',
 
     colorPrimary: 'hsl(0, 7%, 97%)',
     colorPrimaryInverted: 'hsl(0, 7%, 3%)',
@@ -45,17 +45,17 @@ const dewiki: Theme = {
     colorTertiaryInverted: 'hsl(0, 7%, 33%)',
 
 
-    boxShadowUmbra: '0px 3px 6px 0px hsla(0, 89%, 10%, 0.8)',
-    boxShadowUmbraColor: 'hsla(0, 89%, 10%, 0.8)',
-    boxShadowUmbraInset: 'inset 0px -3px 4px 0px hsla(0, 89%, 10%, 0.9)',
+    boxShadowUmbra: '0px 3px 6px 0px hsla(0, 81%, 10%, 0.8)',
+    boxShadowUmbraColor: 'hsla(0, 81%, 10%, 0.8)',
+    boxShadowUmbraInset: 'inset 0px -3px 4px 0px hsla(0, 81%, 10%, 0.9)',
 
-    boxShadowPenumbra: '0px 3px 12px 0px hsla(0, 89%, 10%, 0.8)',
-    boxShadowPenumbraColor: 'hsla(0, 89%, 10%, 0.8)',
-    boxShadowPenumbraInset: 'inset 0px -3px 6px 0px hsla(0, 89%, 10%, 0.9)',
+    boxShadowPenumbra: '0px 3px 12px 0px hsla(0, 81%, 10%, 0.8)',
+    boxShadowPenumbraColor: 'hsla(0, 81%, 10%, 0.8)',
+    boxShadowPenumbraInset: 'inset 0px -3px 6px 0px hsla(0, 81%, 10%, 0.9)',
 
-    boxShadowAntumbra: '0px 3px 18px 0px hsla(0, 89%, 10%, 0.8)',
-    boxShadowAntumbraColor: 'hsla(0, 89%, 10%, 0.8)',
-    boxShadowAntumbraInset: 'inset 0px -3px 8px 0px hsla(0, 89%, 10%, 0.9)',
+    boxShadowAntumbra: '0px 3px 18px 0px hsla(0, 81%, 10%, 0.8)',
+    boxShadowAntumbraColor: 'hsla(0, 81%, 10%, 0.8)',
+    boxShadowAntumbraInset: 'inset 0px -3px 8px 0px hsla(0, 81%, 10%, 0.9)',
 };
 
 
