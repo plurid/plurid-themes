227b404027f0045c955dcdd5657d6c977b72b692
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-08 06:54:47 +0200

    dev: product

---
+++ source/themes/product/deleaf.ts
@@ -0,0 +1,62 @@
+import {
+    Theme,
+} from '../../';
+
+import {
+    THEME_NAME_DELEAF,
+    THEME_TYPE_DARK,
+} from '../../constants';
+
+
+
+const deleaf: Theme = {
+    type: THEME_TYPE_DARK,
+    name: THEME_NAME_DELEAF,
+
+    baseColor: 'hsl(259, 59%, 15%)',
+    baseColorInverted: 'hsl(259, 59%, 85%)',
+
+    backgroundColorDark: 'hsl(259, 59%, 10%)',
+    backgroundColorBright: 'hsl(259, 59%, 90%)',
+
+    backgroundColorPrimary: 'hsl(259, 59%, 15%)',
+    backgroundColorPrimaryAlpha: 'hsla(259, 59%, 15%, 0.4)',
+    backgroundColorPrimaryInverted: 'hsl(259, 59%, 85%)',
+
+    backgroundColorSecondary: 'hsl(259, 59%, 25%)',
+    backgroundColorSecondaryAlpha: 'hsla(259, 59%, 25%, 0.4)',
+    backgroundColorSecondaryInverted: 'hsl(259, 59%, 75%)',
+
+    backgroundColorTertiary: 'hsl(259, 59%, 35%)',
+    backgroundColorTertiaryAlpha: 'hsla(259, 59%, 35%, 0.4)',
+    backgroundColorTertiaryInverted: 'hsl(259, 59%, 75%)',
+
+    backgroundColorQuaternary: 'hsl(259, 59%, 45%)',
+    backgroundColorQuaternaryAlpha: 'hsla(259, 59%, 45%, 0.4)',
+    backgroundColorQuaternaryInverted: 'hsl(259, 59%, 55%)',
+
+    colorPrimary: 'hsl(259, 59%, 97%)',
+    colorPrimaryInverted: 'hsl(259, 59%, 3%)',
+
+    colorSecondary: 'hsl(259, 59%, 77%)',
+    colorSecondaryInverted: 'hsl(259, 59%, 23%)',
+
+    colorTertiary: 'hsl(259, 59%, 67%)',
+    colorTertiaryInverted: 'hsl(259, 59%, 33%)',
+
+
+    boxShadowUmbra: '0px 3px 6px 0px hsla(259, 59%, 10%, 0.8)',
+    boxShadowUmbraColor: 'hsla(259, 59%, 10%, 0.8)',
+    boxShadowUmbraInset: 'inset 0px -3px 4px 0px hsla(259, 59%, 10%, 0.9)',
+
+    boxShadowPenumbra: '0px 3px 12px 0px hsla(259, 59%, 10%, 0.8)',
+    boxShadowPenumbraColor: 'hsla(259, 59%, 10%, 0.8)',
+    boxShadowPenumbraInset: 'inset 0px -3px 6px 0px hsla(259, 59%, 10%, 0.9)',
+
+    boxShadowAntumbra: '0px 3px 18px 0px hsla(259, 59%, 10%, 0.8)',
+    boxShadowAntumbraColor: 'hsla(259, 59%, 10%, 0.8)',
+    boxShadowAntumbraInset: 'inset 0px -3px 8px 0px hsla(259, 59%, 10%, 0.9)',
+};
+
+
+export default deleaf;
---
+++ source/themes/product/detime.ts
@@ -0,0 +1,62 @@
+import {
+    Theme,
+} from '../../';
+
+import {
+    THEME_NAME_DETIME,
+    THEME_TYPE_DARK,
+} from '../../constants';
+
+
+
+const detime: Theme = {
+    type: THEME_TYPE_DARK,
+    name: THEME_NAME_DETIME,
+
+    baseColor: 'hsl(199, 87%, 25%)',
+    baseColorInverted: 'hsl(199, 87%, 75%)',
+
+    backgroundColorDark: 'hsl(199, 87%, 10%)',
+    backgroundColorBright: 'hsl(199, 87%, 90%)',
+
+    backgroundColorPrimary: 'hsl(199, 87%, 25%)',
+    backgroundColorPrimaryAlpha: 'hsla(199, 87%, 25%, 0.4)',
+    backgroundColorPrimaryInverted: 'hsl(199, 87%, 75%)',
+
+    backgroundColorSecondary: 'hsl(199, 87%, 35%)',
+    backgroundColorSecondaryAlpha: 'hsla(199, 87%, 35%, 0.4)',
+    backgroundColorSecondaryInverted: 'hsl(199, 87%, 65%)',
+
+    backgroundColorTertiary: 'hsl(199, 87%, 45%)',
+    backgroundColorTertiaryAlpha: 'hsla(199, 87%, 45%, 0.4)',
+    backgroundColorTertiaryInverted: 'hsl(199, 87%, 55%)',
+
+    backgroundColorQuaternary: 'hsl(199, 87%, 55%)',
+    backgroundColorQuaternaryAlpha: 'hsla(199, 87%, 55%, 0.4)',
+    backgroundColorQuaternaryInverted: 'hsl(199, 87%, 45%)',
+
+    colorPrimary: 'hsl(199, 87%, 97%)',
+    colorPrimaryInverted: 'hsl(199, 87%, 3%)',
+
+    colorSecondary: 'hsl(199, 87%, 77%)',
+    colorSecondaryInverted: 'hsl(199, 87%, 23%)',
+
+    colorTertiary: 'hsl(199, 87%, 67%)',
+    colorTertiaryInverted: 'hsl(199, 87%, 33%)',
+
+
+    boxShadowUmbra: '0px 3px 6px 0px hsla(199, 87%, 10%, 0.8)',
+    boxShadowUmbraColor: 'hsla(199, 87%, 10%, 0.8)',
+    boxShadowUmbraInset: 'inset 0px -3px 4px 0px hsla(199, 87%, 10%, 0.9)',
+
+    boxShadowPenumbra: '0px 3px 12px 0px hsla(199, 87%, 10%, 0.8)',
+    boxShadowPenumbraColor: 'hsla(199, 87%, 10%, 0.8)',
+    boxShadowPenumbraInset: 'inset 0px -3px 6px 0px hsla(199, 87%, 10%, 0.9)',
+
+    boxShadowAntumbra: '0px 3px 18px 0px hsla(199, 87%, 10%, 0.8)',
+    boxShadowAntumbraColor: 'hsla(199, 87%, 10%, 0.8)',
+    boxShadowAntumbraInset: 'inset 0px -3px 8px 0px hsla(199, 87%, 10%, 0.9)',
+};
+
+
+export default detime;
