fe245acaa72532488a94b47ad68e0dd6c6b1c0a9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-11 23:40:01 +0200

    dev: generate color

--- source/themes/generate/utilities.ts
+++ source/themes/generate/utilities.ts
@@ -6,22 +6,22 @@ import {
 
 
 
-const saturationRE = /\((\d{1,3})\s?,/;
-const hueRE = /,\s?(\d{1,3})%\s?,/;
+const hueRE = /\((\d{1,3})\s?,/;
+const saturationRE = /,\s?(\d{1,3})%\s?,/;
 const lightnessRE = /,\s?(\d{1,3})%\)/;
 
 export const parseHSL = (color: string): HSLColor | undefined => {
-    const saturationMatch = color.match(saturationRE);
     const hueMatch = color.match(hueRE);
+    const saturationMatch = color.match(saturationRE);
     const lightnessMatch = color.match(lightnessRE);
 
-    if (!saturationMatch || !hueMatch || !lightnessMatch) {
+    if (!hueMatch || !saturationMatch || !lightnessMatch) {
         return;
     }
 
     const colorValues: HSLColorValues = {
-        saturation: parseInt(saturationMatch[1]),
         hue: parseInt(hueMatch[1]),
+        saturation: parseInt(saturationMatch[1]),
         lightness: parseInt(lightnessMatch[1]),
     };
     const hslColor = new HSLColor(colorValues);
@@ -32,8 +32,8 @@ export const parseHSL = (color: string): HSLColor | undefined => {
 
 export const invertColor = (color: HSLColor): HSLColor => {
     const colorValues: HSLColorValues = {
-        saturation: color.saturation(),
         hue: color.hue(),
+        saturation: color.saturation(),
         lightness: 100 - color.lightness(),
     };
     const hslColor = new HSLColor(colorValues);
